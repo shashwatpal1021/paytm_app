cd9a428f79481ea7bc049f2f884797f885985e0b
shashwat pal <shashwatpal1021@gmail.com>
2024-03-31 00:16:49 +0530

    setup

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -1,4 +1,7 @@
 
+import { PrismaClient } from "@repo/db/client";
+
+const client = new PrismaClient();
 export default function Page(): JSX.Element {
   return (
     <div className="text-blue-500 text-5xl bg-black">
